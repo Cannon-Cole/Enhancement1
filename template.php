948df3983be4d4b988c6343c9871e4a8b5fa5df8
Cole <2bigshot@25.3.110.62>
2018-09-28 22:17:12 -0600

    Template

---
+++ template.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/main.css" media="screen">
+
+    </head>
+
+    <body>
+
+        <header>
+            <img id="logo" src="images/site/logo.gif">
+            <div class="my-account">
+                <img src="images/site/account.gif">
+                <p>My Account</p>
+            </div>
+        </header>
+
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/nav.php'; ?> 
+        
+        <main>
+            
+            <h1>Content here</h1>
+            
+        </main>
+
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/footer.php'; ?>
+        
+    </body>
+
+</html>
